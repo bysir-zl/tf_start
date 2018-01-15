e19ffcbb45dad566f05700fd11f884e1645b91b6
zhangliang <zhangliang@zhuzi.me>
2018-01-15 19:00:29 +0800

    nothing

--- mnist/mnist_.py
+++ mnist/mnist_.py
@@ -8,6 +8,11 @@ mnist = input_data.read_data_sets("data/", one_hot=True)
 x = tf.placeholder("float", [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
+# 为了得到一张给定图片属于某个特定数字类的证据（evidence），我们对图片像素值进行加权求和。
+# 如果这个像素具有很强的证据说明这张图片不属于该类，那么相应的权值为负数，相反如果这个像素拥有有利的证据支持这张图片属于这个类，那么权值是正数。
+# softmax: 按概率选择最大的值, 越大选中的概率越大
+# 加权求和, 得到最大概率
+# 这里用到了 矩阵乘法, 得到的是10*10的矩阵, 在加上
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder("float", [None, 10])
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
@@ -19,9 +24,9 @@ sess.run(init)
 
 for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    sess.run(train_step, {x: batch_xs, y_: batch_ys})
 
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+print(sess.run(accuracy, {x: mnist.test.images, y_: mnist.test.labels}))
